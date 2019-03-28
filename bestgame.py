c930c8c6699d4a2ff93530b0273e0cfd3a7d3ebf
johntonsoup <42421680+johntonsoup@users.noreply.github.com>
2019-03-28 11:14:53 -0700

    Add files via upload

---
+++ bestgame.py
@@ -0,0 +1,25 @@
+print("Welcome to Tic-Tac-Toe!")
+
+board = [
+    [ '1','2','3'],
+    [ '4','5','6'],
+    [ '7','8','9']
+    ]
+
+def print_board():
+    for g in board:
+        print(g)
+        
+
+print_board()
+
+select = input("pick position : ")
+
+p1 = "x"
+p2 = "o"
+
+
+
+
+
+
